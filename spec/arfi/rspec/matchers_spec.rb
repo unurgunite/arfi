11fff6ca9f7e3ff8f65b22c09ad28f31a1d29525
unurgunite <senpaiguru1488@gmail.com>
2026-06-14 01:13:05 +0300

    [1.1.0] Fix RSpec/ExpectActual for rubocop-rspec 3.10

--- spec/arfi/rspec/matchers_spec.rb
+++ spec/arfi/rspec/matchers_spec.rb
@@ -5,21 +5,26 @@ require 'arfi/rspec/matchers'
 
 RSpec.describe 'ARFI RSpec matchers', :pgsql do # rubocop:disable RSpec/DescribeClass
   describe 'have_arfi_function' do
+    let(:function_name) { 'arfi_test_fn' }
+
     it 'passes when the function exists in the database' do
       ActiveRecord::Base.connection.execute(<<~SQL)
         CREATE OR REPLACE FUNCTION public.arfi_test_fn()
         RETURNS INT LANGUAGE SQL AS $$ SELECT 1; $$;
       SQL
 
-      expect('arfi_test_fn').to have_arfi_function
+      expect(function_name).to have_arfi_function
     end
 
     it 'fails when the function does not exist' do
-      expect('arfi_nonexistent_fn').not_to have_arfi_function
+      missing_fn = 'arfi_nonexistent_fn'
+      expect(missing_fn).not_to have_arfi_function
     end
   end
 
   describe 'have_arfi_trigger' do
+    let(:trigger_name) { 'arfi_test_trg' }
+
     it 'passes when the trigger exists on the given table' do # rubocop:disable RSpec/ExampleLength
       conn = ActiveRecord::Base.connection
       conn.execute(<<~SQL)
@@ -40,15 +45,17 @@ RSpec.describe 'ARFI RSpec matchers', :pgsql do # rubocop:disable RSpec/Describe
           EXECUTE FUNCTION public.arfi_test_trg_fn();
       SQL
 
-      expect('arfi_test_trg').to have_arfi_trigger.on_table('arfi_test_trg_users')
+      expect(trigger_name).to have_arfi_trigger.on_table('arfi_test_trg_users')
     end
 
     it 'fails when the trigger does not exist' do
-      expect('arfi_nonexistent_trg').not_to have_arfi_trigger.on_table('arfi_test_trg_users')
+      missing_trg = 'arfi_nonexistent_trg'
+      expect(missing_trg).not_to have_arfi_trigger.on_table('arfi_test_trg_users')
     end
 
     it 'raises ArgumentError when .on_table is not called' do # rubocop:disable RSpec/MultipleExpectations
-      expect { expect('any_trg').to have_arfi_trigger }
+      any_trg = 'any_trg'
+      expect { expect(any_trg).to have_arfi_trigger }
         .to raise_error(ArgumentError, /on_table/)
     end
   end
